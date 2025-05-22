dcc9d1159efb4f528f585239df35c2e408c4e6cd
Bea <ab291012@gmail.com>
2025-05-22 16:26:55 -0300

    feat: refactor

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -5,14 +5,9 @@ CREATE DATABASE aulas;
 CREATE TABLE professores (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(70) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
+    email VARCHAR(100) UNIQUE NOT NULL, 
+    photo TEXT
 );
-
-INSERT INTO professores (nome, email) VALUES 
-    ('Thiago Ferreira', 'thiago@email.com'),
-    ('Marcelo Carboni', 'marcelo@email.com');
-
-ALTER TABLE professores ADD COLUMN photo TEXT;
 
 SELECT * FROM professores;
 
@@ -22,23 +17,24 @@ CREATE TABLE disciplinas (
     professores_id INTEGER REFERENCES professores(id) NOT NULL
 );
 
-INSERT INTO disciplinas (professores_id, materia) VALUES 
-(1, 'Back-End'),
-(2, 'Mobile');
-
 
 SELECT * FROM disciplinas;
 
-INSERT INTO professores (nome, email) VALUES 
-    ('Pereira', 'pereirao@email.com'),
-    ('Ivonete', 'nenete@email.com'),
+INSERT INTO professores (nome, email, photo) VALUES 
+    ('Thiago Ferreira', 'thiago@email.com', '/images/thiago.jpg'),
+    ('Marcelo Carboni', 'marcelo@email.com', '/images/marcello.jpg'),
+    ('Pereira', 'pereirao@email.com', '/images/pereira.jpg'),
+    ('Ivonete', 'nenete@email.com', '/images/ivonete.jpg'),
+    ('Alecio Cau', 'aleciao@email.com', '/images/alecio.jpg'),
+    ('Tamires', 'tamires@email.com', '/images/tamires.jpg'),
+    ('Daniela', 'dani@email.com', '/images/dani.jpg'),
+    ('Gatinho Dev', 'gatinho@email.com', '/images/gatinho.jpg');
 
-    INSERT INTO professores (nome, email) VALUES 
-    ('Alécio Cau', 'aleciao@email.com'),
-    ('Tamires', 'tamires@email.com');
 
     INSERT INTO disciplinas (professores_id, materia) VALUES 
-(5, 'Matemática'),
-(6, 'Português'),
-(9, 'Física'),
-(10, 'Biologia');
+    (1, 'Back-End'),
+    (2, 'Mobile'),
+    (3, 'Matemática'),
+    (4, 'Português'),
+    (5, 'Física'),
+    (6, 'Biologia');
